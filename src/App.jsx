c0febccde976c5dca0748cb6fb65b38cb21bf5d3
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2024-01-31 12:48:28 +0900

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import { Router } from "./router/Router";
+
+export const App = () => {
+  return <Router />;
+};
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { SearchInput } from "../molecules/SearchInput";
+import { UserCard } from "../organism/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `たかき${val}`,
+    image: "https://source.unsplash.com/JBrbzg5N7Go",
+    email: "takaki55730317@gmail.com",
+    phone: "090-1438-2914",
+    company: {
+      name: "テスト株式会社"
+    },
+    website: "https://yahoo.com"
+  };
+});
+export const Users = () => {
+  return (
+    <SContainer>
+      <h1>ユーザー一覧</h1>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
